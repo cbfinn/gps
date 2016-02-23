5b6ee4f52ed2fdc4e4645c61cdfed5756a11dd39
Chelsea Finn <cbfinn@eecs.berkeley.edu>
2016-02-23 08:58:19 -0800

    read the docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,12 @@
 import sys
 import os
 import shlex
-import sphinx_bootstrap_theme
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+  import sphinx_bootstrap_theme
+  html_theme = 'bootstrap'
+  html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -113,8 +118,6 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'alabaster'
-html_theme = 'bootstrap'
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
