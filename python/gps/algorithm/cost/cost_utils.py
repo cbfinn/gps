7ac580d271cf20e2ff41855ff5bedf33d026544a
Chelsea Finn <chelseaf@google.com>
2016-07-07 09:40:40 -0700

    added some changes that need to be tested

--- python/gps/algorithm/cost/cost_utils.py
+++ python/gps/algorithm/cost/cost_utils.py
@@ -244,3 +244,102 @@ def construct_quad_cost_net(dim_hidden=None, dim_input=27, T=100,
                                            layer='IOCLoss'))
 
     return n.to_proto()
+
+
+def construct_nn_cost_net(num_hidden=3, dim_hidden=None, dim_input=27, T=100,
+                          demo_batch_size=5, sample_batch_size=5, phase=TRAIN):
+    """
+    Construct an anonymous network (no layer names) for a quadratic cost
+    function with the specified dimensionality, and return NetParameter proto.
+
+    Note: this function is an example for how one might want to specify
+    their network, versus providing a protoxt model file. It is not
+    meant to be a general solution for specifying any network.
+
+    Args:
+        num_hidden: Number of hidden layers.
+        dim_hidden: Dimensionality of hidden layer.
+        dim_input: Dimensionality of input.
+        T: time horizon
+        demo_batch_size: demo batch size.
+        sample_batch_size: sample batch size.
+        phase: TRAIN, TEST, or 'forward_feat'
+    Returns:
+        A NetParameter specification of the network.
+    """
+    if dim_hidden is None:
+        dim_hidden = 42
+
+    n = caffe.NetSpec()
+
+    # Needed for Caffe to find defined python layers.
+    sys.path.append('/'.join(str.split(current_path, '/')[:-1]))
+    if phase == TRAIN:
+        data_layer_info = json.dumps({
+            'shape': [{'dim': (demo_batch_size, T, dim_input)},
+                      {'dim': (demo_batch_size, 1)},
+                      {'dim': (sample_batch_size, T, dim_input)},
+                      {'dim': (sample_batch_size, 1)}]
+        })
+
+        [n.demos, n.d_log_iw, n.samples, n.s_log_iw] = L.Python(
+            ntop=4, python_param=dict(
+                module='ioc_layers', param_str=data_layer_info,
+                layer='IOCDataLayer'
+            )
+        )
+        n.net_input = L.Concat(n.demos, n.samples, axis=0)
+    elif phase == TEST or phase == 'forward_feat':
+        data_layer_info = json.dumps({
+            'shape': [{'dim': (1, T, dim_input)}]
+        })
+        n.net_input = L.Python(ntop=1,
+                               python_param=dict(module='ioc_layers',
+                                                 param_str=data_layer_info,
+                                                 layer='IOCDataLayer'))
+    else:
+        raise Exception('Unknown network phase')
+
+    n.layer = n.net_input
+    for i in range(dim_hidden):
+        n.layer = L.InnerProduct(n.layer, num_output=dim_hidden,
+                                 weight_filler=dict(type='gaussian', std=0.01),
+                                 bias_filler=dict(type='constant', value=0),
+                                 axis=2)
+        n.layer = L.ReLU(n.layer, in_place=True)
+    n.feat = n.layer
+
+
+    if phase != 'forward_feat':
+        n.Ax = L.InnerProduct(n.feat, num_output=dim_hidden,
+                              weight_filler=dict(type='gaussian', std=0.01),
+                              bias_filler=dict(type='constant', value=0),
+                              axis=2)
+
+        # Dot product operation with two layers
+        n.AxAx = L.Eltwise(n.Ax, n.Ax, operation=EltwiseParameter.PROD)
+        n.all_costs = L.InnerProduct(n.AxAx, num_output=1, axis=2,
+                                     weight_filler=dict(type='constant', value=1),
+                                     bias_filler=dict(type='constant', value=0),
+                                     param=[dict(lr_mult=0), dict(lr_mult=0)])
+
+    if phase == TRAIN:
+        n.demo_costs, n.sample_costs = L.Slice(n.all_costs, axis=0, slice_point=demo_batch_size, ntop=2)
+
+        # regularization
+        n.costs_prev, _ = L.Slice(n.all_costs, axis=1, slice_point=T-2, ntop=2)
+        _, n.costs_next = L.Slice(n.all_costs, axis=1, slice_point=2, ntop=2)
+        _, n.costs_cur, _ = L.Slice(n.all_costs, axis=1, slice_point=[1,T-1], ntop=3)
+        # cur-prev
+        n.slope_prev = L.Eltwise(n.costs_cur, n.costs_prev, operation=EltwiseParameter.SUM, coeff=[1,-1])
+        # next-cur
+        n.slope_next = L.Eltwise(n.costs_next, n.costs_cur, operation=EltwiseParameter.SUM, coeff=[1,-1])
+        # TODO - add hyperparam for loss weight.
+        n.reg = L.EuclideanLoss(n.slope_next, n.slope_prev, loss_weight=0.1)
+
+        n.out = L.Python(n.demo_costs, n.sample_costs, n.d_log_iw, n.s_log_iw, loss_weight=1.0,
+                         python_param=dict(module='ioc_layers',
+                                           layer='IOCLoss'))
+
+    return n.to_proto()
+
