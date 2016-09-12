7fe3ce05c325d9847e966214e4c603d75909b45f
Justin Fu <justinjfu@gmail.com>
2016-09-12 09:23:40 -0700

    Supervised weight copy

--- python/gps/algorithm/cost/cost_ioc_supervised.py
+++ python/gps/algorithm/cost/cost_ioc_supervised.py
@@ -3,6 +3,7 @@ import copy
 import logging
 import numpy as np
 import tempfile
+from os.path import join
 
 import caffe
 from caffe.proto.caffe_pb2 import SolverParameter, TRAIN, TEST
@@ -24,8 +25,15 @@ class CostIOCSupervised(CostIOCNN):
         self.gt_cost = self.gt_cost['type'](self.gt_cost)
         self.agent = hyperparams['agent']  # Required for sample packing
         self.agent = self.agent['type'](self.agent)
+        self.weights_dir = hyperparams['weight_dir']
+        self.weight_file = join(self.weight_dir, 'supervised_net.net')
+
+        solver = init_solver(phase='supervised')
         if hyperparams.get('init_from_demos', True):
             self.init_supervised_demos(hyperparams['demo_file'])
+        solver = init_solver(phase=TRAIN)
+        solver.net.copy_from(self.weight_file)  # Load weights into train net
+
         self.finetune = hyperparams.get('finetune', False)
 
     def update(self, demoU, demoX, demoO, d_log_iw, sampleU, sampleX, sampleO, s_log_iw):
@@ -52,7 +60,7 @@ class CostIOCSupervised(CostIOCNN):
         self.init_supervised(U, X, O)
 
 
-    def init_supervised(self, sampleU, sampleX, sampleO):
+    def init_supervised(self, solver, sampleU, sampleX, sampleO):
         """
         """
 
@@ -61,7 +69,7 @@ class CostIOCSupervised(CostIOCNN):
 
         sample_batch_size = self.sample_batch_size + self.demo_batch_size
 
-        blob_names = self.solver.net.blobs.keys()
+        blob_names = solver.net.blobs.keys()
         sbatches_per_epoch = np.floor(Ns / sample_batch_size)
 
         sample_idx = range(Ns)
@@ -83,11 +91,11 @@ class CostIOCSupervised(CostIOCNN):
           s_start_idx = int(i * sample_batch_size %
               (sbatches_per_epoch * sample_batch_size))
           s_idx_i = sample_idx[s_start_idx:s_start_idx+sample_batch_size]
-          self.solver.net.blobs[blob_names[0]].data[:] = sampleO[s_idx_i]
-          self.solver.net.blobs[blob_names[1]].data[:] = np.sum(self._hyperparams['wu']*sampleU[s_idx_i]**2, axis=2, keepdims=True)
-          self.solver.net.blobs[blob_names[2]].data[:] = sample_costs[s_idx_i]
-          self.solver.step(1)
-          train_loss = self.solver.net.blobs[blob_names[-1]].data
+          solver.net.blobs[blob_names[0]].data[:] = sampleO[s_idx_i]
+          solver.net.blobs[blob_names[1]].data[:] = np.sum(self._hyperparams['wu']*sampleU[s_idx_i]**2, axis=2, keepdims=True)
+          solver.net.blobs[blob_names[2]].data[:] = sample_costs[s_idx_i]
+          solver.step(1)
+          train_loss = solver.net.blobs[blob_names[-1]].data
           average_loss += train_loss
           if i % 500 == 0 and i != 0:
             LOGGER.debug('Caffe iteration %d, average loss %f',
@@ -95,9 +103,66 @@ class CostIOCSupervised(CostIOCNN):
             average_loss = 0
         import pdb; pdb.set_trace()
 
-
         # Keep track of Caffe iterations for loading solver states.
         self.caffe_iter += self._hyperparams['iterations']
-        self.solver.test_nets[0].share_with(self.solver.net)
-        self.solver.test_nets[1].share_with(self.solver.net)
+        solver.test_nets[0].share_with(solver.net)
+        solver.test_nets[1].share_with(solver.net)
 
+        solver.save(self.weight_file)
+
+
+    def init_solver(self, phase='supervised', sample_batch_size=None):
+        """ Helper method to initialize the solver. """
+        solver_param = SolverParameter()
+        solver_param.display = 0  # Don't display anything.
+        solver_param.base_lr = self._hyperparams['lr']
+        solver_param.lr_policy = self._hyperparams['lr_policy']
+        solver_param.momentum = self._hyperparams['momentum']
+        solver_param.weight_decay = self._hyperparams['weight_decay']
+        solver_param.type = self._hyperparams['solver_type']
+        solver_param.random_seed = self._hyperparams['random_seed']
+
+        # Pass in net parameter by protostring (could add option to input prototxt file).
+        network_arch_params = self._hyperparams['network_arch_params']
+
+        network_arch_params['dim_input'] = self._dO
+        network_arch_params['demo_batch_size'] = self._hyperparams['demo_batch_size']
+        if sample_batch_size is None:
+            network_arch_params['sample_batch_size'] = self._hyperparams['sample_batch_size']
+        else:
+            network_arch_params['sample_batch_size'] = sample_batch_size
+        network_arch_params['T'] = self._T
+        network_arch_params['phase'] = TRAIN
+        network_arch_params['ioc_loss'] = self._hyperparams['ioc_loss']
+        network_arch_params['Nq'] = self._iteration_count
+        network_arch_params['smooth_reg_weight'] = self._hyperparams['smooth_reg_weight']
+        network_arch_params['mono_reg_weight'] = self._hyperparams['mono_reg_weight']
+        network_arch_params['gp_reg_weight'] = self._hyperparams['gp_reg_weight']
+        network_arch_params['learn_wu'] = self._hyperparams['learn_wu']
+        network_arch_params['phase'] = phase
+        solver_param.train_net_param.CopyFrom(
+            self._hyperparams['network_model'](**network_arch_params)
+        )
+
+        # For running forward in python.
+        network_arch_params['phase'] = TEST
+        solver_param.test_net_param.add().CopyFrom(
+            self._hyperparams['network_model'](**network_arch_params)
+        )
+
+        network_arch_params['phase'] = 'forward_feat'
+        solver_param.test_net_param.add().CopyFrom(
+            self._hyperparams['network_model'](**network_arch_params)
+        )
+
+        # These are required by Caffe to be set, but not used.
+        solver_param.test_iter.append(1)
+        solver_param.test_iter.append(1)
+        solver_param.test_interval = 1000000
+
+        f = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        f.write(MessageToString(solver_param))
+        f.close()
+        self.solver = caffe.get_solver(f.name)
+        self.caffe_iter = 0
+        return self.solver
