88a021d062fde0d443ebd2aca17b7f2d2738f870
Tianhe Yu <tianhe.yu@berkeley.edu>
2016-09-23 12:01:06 -0700

    inital setup of pointmass obstacle

--- python/gps/agent/mjc/mjc_models.py
+++ python/gps/agent/mjc/mjc_models.py
@@ -99,7 +99,7 @@ def colored_reacher(ncubes=6, target_color="red", cube_size=0.015):
     actuator.motor(ctrllimited="true",ctrlrange="-1.0 1.0",gear="200.0",joint="joint1")
     return mjcmodel
 
-def pointmass():
+def obstacle_pointmass():
     """
     An example usage of MJCModel building the pointmass task
 
@@ -121,4 +121,13 @@ def pointmass():
     body.geom(name="target_geom", type="capsule", fromto="-0.01 0 0 0.01 0 0", size="0.1", rgba="0 0.9 0.1 1")
 
     # Walls
+    body = worldbody.body(name="wall1", pos="1 0.8 0")
+    body.geom(name="wall1_geom", type="capsule", fromto="0 -0.6 0 0 1.0 0.0", size="0.1", contype="1", rgba="0.9 0 0.1 1")
+    body = worldbody.body(name="wall2", pos="1 -0.8 0")
+    body.geom(name="wall2_geom", type="capsule", fromto="0 -1.0 0 0 0.6 0.0", size="0.1", contype="1", rgba="0.9 0 0.1 1")
 
+    # Actuators
+    actuator = mjcmodel.root.actuator()
+    actuator.motor(joint="ball_x", ctrlrange="-1.0 1.0", ctrllimited="true")
+    actuator.motor(joint="ball_y", ctrlrange="-1.0 1.0", ctrllimited="true")
+    return mjcmodel
