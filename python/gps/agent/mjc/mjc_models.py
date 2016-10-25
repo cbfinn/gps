86f4a72425990b49b10c3b0ecdda14bc05826a65
Tianhe Yu <tianhe.yu@berkeley.edu>
2016-10-24 17:23:50 -0700

    set up weight-varying reacher experiment

--- python/gps/agent/mjc/mjc_models.py
+++ python/gps/agent/mjc/mjc_models.py
@@ -54,6 +54,48 @@ def reacher():
 
     return mjcmodel
 
+def weighted_reacher(density=1.0):
+    """
+    An example usage of MJCModel building the weighted reacher task
+    Args:
+        density: the density of the fingertip
+    Returns:
+        An MJCModel
+    """
+    mjcmodel = default_model('reacher')
+    worldbody = mjcmodel.root.worldbody()
+
+    # Arena
+    worldbody.geom(conaffinity="0",fromto="-.3 -.3 .01 .3 -.3 .01",name="sideS",rgba="0.9 0.4 0.6 1",size=".02",type="capsule")
+    worldbody.geom(conaffinity="0",fromto=" .3 -.3 .01 .3  .3 .01",name="sideE",rgba="0.9 0.4 0.6 1",size=".02",type="capsule")
+    worldbody.geom(conaffinity="0",fromto="-.3  .3 .01 .3  .3 .01",name="sideN",rgba="0.9 0.4 0.6 1",size=".02",type="capsule")
+    worldbody.geom(conaffinity="0",fromto="-.3 -.3 .01 -.3 .3 .01",name="sideW",rgba="0.9 0.4 0.6 1",size=".02",type="capsule")
+
+    # Arm
+    worldbody.geom(conaffinity="0",contype="0",fromto="0 0 0 0 0 0.02",name="root",rgba="0.9 0.4 0.6 1",size=".011",type="cylinder")
+    body = worldbody.body(name="body0", pos="0 0 .01")
+    body.geom(fromto="0 0 0 0.1 0 0",name="link0",rgba="0.0 0.4 0.6 1",size=".01",type="capsule")
+    # body.joint(axis="0 0 1",limited="false",name="joint0",pos="0 0 0",type="hinge")
+    body.joint(axis="0 0 1",limited="true",name="joint0",pos="0 0 0",range="-3.14 3.14",type="hinge")
+    body = body.body(name="body1",pos="0.1 0 0")
+    body.joint(axis="0 0 1",limited="true",name="joint1",pos="0 0 0",range="-3.0 3.0",type="hinge")
+    body.geom(fromto="0 0 0 0.1 0 0",name="link1",rgba="0.0 0.4 0.6 1",size=".01",type="capsule")
+    body = body.body(name="fingertip",pos="0.11 0 0")
+    body.site(name="fingertip",pos="0 0 0",size="0.01")
+    body.geom(contype="0",name="fingertip",pos="0 0 0",rgba="0.0 0.8 0.6 1",size=".01",type="sphere", density=density)
+
+    # Target
+    body = worldbody.body(name="target",pos=".1 -.1 .01")
+    body.geom(rgba="1. 0. 0. 1",type="box",size="0.01 0.01 0.01",density='0.00001',contype="0",conaffinity="0")
+    body.site(name="target",pos="0 0 0",size="0.01")
+
+    # Actuators
+    actuator = mjcmodel.root.actuator()
+    actuator.motor(ctrllimited="true",ctrlrange="-1.0 1.0",gear="200.0",joint="joint0")
+    actuator.motor(ctrllimited="true",ctrlrange="-1.0 1.0",gear="200.0",joint="joint1")
+
+    return mjcmodel
+
 
 def colored_reacher(ncubes=6, target_color="red", cube_size=0.015):
     mjcmodel = default_model('reacher')
