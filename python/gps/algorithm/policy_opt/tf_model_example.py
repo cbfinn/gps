ee4ce6d70301c1a260dce3d0b152a9be095dce47
Chelsea Finn <cfinnswim@gmail.com>
2016-10-31 00:40:08 -0700

    bug fix

--- python/gps/algorithm/policy_opt/tf_model_example.py
+++ python/gps/algorithm/policy_opt/tf_model_example.py
@@ -83,10 +83,11 @@ def example_tf_network(dim_input=27, dim_output=7, batch_size=25, network_config
     dim_hidden.append(dim_output)
 
     nn_input, action, precision = get_input_layer(dim_input, dim_output)
-    mlp_applied = get_mlp_layers(nn_input, n_layers, dim_hidden)
+    mlp_applied, weights_FC, biases_FC = get_mlp_layers(nn_input, n_layers, dim_hidden)
+    fc_vars = weights_FC + biases_FC
     loss_out = get_loss_layer(mlp_out=mlp_applied, action=action, precision=precision, batch_size=batch_size)
 
-    return TfMap.init_from_lists([nn_input, action, precision], [mlp_applied], [loss_out])
+    return TfMap.init_from_lists([nn_input, action, precision], [mlp_applied], [loss_out]), fc_vars, []
 
 
 def multi_modal_network(dim_input=27, dim_output=7, batch_size=25, network_config=None):
@@ -158,7 +159,7 @@ def multi_modal_network(dim_input=27, dim_output=7, batch_size=25, network_confi
 
     fc_input = tf.concat(concat_dim=1, values=[conv_out_flat, state_input])
 
-    fc_output = get_mlp_layers(fc_input, n_layers, dim_hidden)
+    fc_output, _, _ = get_mlp_layers(fc_input, n_layers, dim_hidden)
 
     loss = euclidean_loss_layer(a=action, b=fc_output, precision=precision, batch_size=batch_size)
     return TfMap.init_from_lists([nn_input, action, precision], [fc_output], [loss])
