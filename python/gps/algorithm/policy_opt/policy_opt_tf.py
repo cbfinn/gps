3cb09a04ab6be4f9f95b79efb152c6d484b057be
Chelsea Finn <cfinnswim@gmail.com>
2016-10-31 17:11:12 -0700

    bug fix

--- python/gps/algorithm/policy_opt/policy_opt_tf.py
+++ python/gps/algorithm/policy_opt/policy_opt_tf.py
@@ -191,7 +191,8 @@ class PolicyOptTf(PolicyOpt):
 
         feed_dict = {self.obs_tensor: obs}
         num_values = obs.shape[0]
-        self.feat_vals = self.solver.get_var_values(self.sess, self.feat_op, feed_dict, num_values, self.batch_size)
+        if self.feat_op is not None:
+            self.feat_vals = self.solver.get_var_values(self.sess, self.feat_op, feed_dict, num_values, self.batch_size)
         # Keep track of tensorflow iterations for loading solver states.
         self.tf_iter += self._hyperparams['iterations']
 
